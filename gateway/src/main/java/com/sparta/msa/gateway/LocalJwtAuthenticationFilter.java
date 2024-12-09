e95bf506d347083ab4ee1b721dd91833f0ac26f9
rua0704 <rua0704@gmail.com>
2024-12-09 11:09:42 +0900

    feat - gateway 생성

---
+++ gateway/src/main/java/com/sparta/msa/gateway/LocalJwtAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.sparta.msa.gateway;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Slf4j
+@Component
+public class LocalJwtAuthenticationFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (path.equals("/auth/signIn") || path.equals("/auth/signUp")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+
+        if(token == null || !validateToken(token, exchange)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+        if (token != null && token.startsWith("Bearer ")) {
+            return token.substring(7);
+        }
+
+        return null;
+    }
+
+    private boolean validateToken(String token, ServerWebExchange exchange) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+            log.info("#####payload :: " + claimsJws.getPayload().toString());
+            Claims claims = claimsJws.getPayload();
+            exchange.getRequest().mutate()
+                    .header("X-Username", claims.get("username").toString())
+                    .build();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
