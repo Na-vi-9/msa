142e84d3777bd257386cb94da03d4bc4f7bd4aa7
rua0704 <rua0704@gmail.com>
2024-12-10 21:26:31 +0900

    feat - 서비스 구현

---
+++ delivery/src/main/java/com/sparta/msa/delivery/application/dto/UpdateDeliveryResponse.java
@@ -0,0 +1,35 @@
+package com.sparta.msa.delivery.application.dto;
+
+import com.sparta.msa.delivery.domain.model.Delivery;
+import com.sparta.msa.delivery.domain.model.DeliveryStatus;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+public class UpdateDeliveryResponse {
+    private UUID uuid;
+    private UUID orderUUID;
+    private UUID departureHubUUID;
+    private UUID arrivalHubUUID;
+    private DeliveryStatus status;
+    private String deliveryAddress;
+    private String receiverName;
+    private String receiverSlackId;
+
+    public static UpdateDeliveryResponse of(Delivery delivery) {
+        return UpdateDeliveryResponse.builder()
+                .uuid(delivery.getUuid())
+                .orderUUID(delivery.getOrderUUID())
+                .departureHubUUID(delivery.getDepartureHubUUID())
+                .arrivalHubUUID(delivery.getArrivalHubUUID())
+                .status(delivery.getStatus())
+                .deliveryAddress(delivery.getDeliveryAddress())
+                .receiverName(delivery.getReceiverName())
+                .receiverSlackId(delivery.getReceiverSlackId())
+                .build();
+    }
+}
--- delivery/src/main/java/com/sparta/msa/delivery/application/service/DeliveryService.java
+++ delivery/src/main/java/com/sparta/msa/delivery/application/service/DeliveryService.java
@@ -2,11 +2,17 @@ package com.sparta.msa.delivery.application.service;
 
 import com.sparta.msa.delivery.application.dto.CreateDeliveryDto;
 import com.sparta.msa.delivery.application.dto.CreateDeliveryResponse;
+import com.sparta.msa.delivery.application.dto.UpdateDeliveryResponse;
+import com.sparta.msa.delivery.application.dto.UpdateDeliveryDto;
 import com.sparta.msa.delivery.domain.model.Delivery;
 import com.sparta.msa.delivery.domain.repository.DeliveryRepository;
+import com.sparta.msa.delivery.infrastructure.exception.CustomException;
+import com.sparta.msa.delivery.infrastructure.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +26,14 @@ public class DeliveryService {
         Delivery delivery = Delivery.create(dto.getOrderUUID(), dto.getStatus(), dto.getDepartureHubUUID(), dto.getArrivalHubUUID(), dto.getDeliveryAddress(), dto.getReceiverName(), dto.getReceiverSlackId());
         return CreateDeliveryResponse.of(deliveryRepository.save(delivery));
 
+    }
+
+    @Transactional
+    public UpdateDeliveryResponse updateDelivery(UUID deliveryUUID, UpdateDeliveryDto request) {
+
+        return deliveryRepository.findByUuid(deliveryUUID).map(delivery -> {
+            delivery.update(request.getOrderUUID(), request.getStatus(), request.getDepartureHubUUID(), request.getArrivalHubUUID(), request.getDeliveryAddress(), request.getReceiverName(), request.getReceiverSlackId());
+            return UpdateDeliveryResponse.of(delivery);
+        }).orElseThrow(() -> new CustomException(ErrorCode.DELIVERY_NOT_FOUND));
     }
 }
