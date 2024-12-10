32146350f0496709298960b4ac882441ee4a15cc
rua0704 <rua0704@gmail.com>
2024-12-10 21:26:50 +0900

    feat - 컨트롤러 구현

--- delivery/src/main/java/com/sparta/msa/delivery/application/dto/DeliveryResponse.java
+++ delivery/src/main/java/com/sparta/msa/delivery/application/dto/DeliveryResponse.java
@@ -1,4 +1,34 @@
 package com.sparta.msa.delivery.application.dto;
 
+import com.sparta.msa.delivery.domain.model.Delivery;
+import com.sparta.msa.delivery.domain.model.DeliveryStatus;
+import lombok.*;
+
+import java.util.UUID;
+@Getter
+@NoArgsConstructor
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class DeliveryResponse {
+    private UUID uuid;
+    private UUID orderUUID;
+    private UUID departureHubUUID;
+    private UUID arrivalHubUUID;
+    private DeliveryStatus status;
+    private String deliveryAddress;
+    private String receiverName;
+    private String receiverSlackId;
+
+    public static DeliveryResponse of(Delivery delivery) {
+        return DeliveryResponse.builder()
+                .uuid(delivery.getUuid())
+                .orderUUID(delivery.getOrderUUID())
+                .departureHubUUID(delivery.getDepartureHubUUID())
+                .arrivalHubUUID(delivery.getArrivalHubUUID())
+                .status(delivery.getStatus())
+                .deliveryAddress(delivery.getDeliveryAddress())
+                .receiverName(delivery.getReceiverName())
+                .receiverSlackId(delivery.getReceiverSlackId())
+                .build();
+    }
 }
--- delivery/src/main/java/com/sparta/msa/delivery/presentation/request/UpdateDeliveryRequest.java
+++ delivery/src/main/java/com/sparta/msa/delivery/presentation/request/UpdateDeliveryRequest.java
@@ -1,4 +1,43 @@
 package com.sparta.msa.delivery.presentation.request;
 
+import com.sparta.msa.delivery.application.dto.UpdateDeliveryDto;
+import com.sparta.msa.delivery.domain.model.DeliveryStatus;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
 public class UpdateDeliveryRequest {
+    @NotNull
+    private UUID orderUUID;
+
+    @NotNull
+    private DeliveryStatus status;
+
+    @NotNull
+    private UUID departureHubUUID;
+
+    @NotNull
+    private UUID arrivalHubUUID;
+
+    @NotNull
+    @Size(max = 255)
+    private String deliveryAddress;
+
+    @NotNull
+    @Size(max = 50)
+    private String receiverName;
+
+    @NotNull
+    @Size(max = 20)
+    private String receiverSlackId;
+
+    public UpdateDeliveryDto toDto() {
+        return UpdateDeliveryDto.create(this.orderUUID, this.status, this.departureHubUUID,
+                this.arrivalHubUUID, this.deliveryAddress, this.receiverName,
+                this.receiverSlackId);
+    }
 }
