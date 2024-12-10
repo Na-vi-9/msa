22c39986d4a49aa69e355c504a2eb33b66f24d73
rua0704 <rua0704@gmail.com>
2024-12-10 21:24:36 +0900

    feat - 레포지토리 구현

--- delivery/src/main/java/com/sparta/msa/delivery/domain/repository/DeliveryRepository.java
+++ delivery/src/main/java/com/sparta/msa/delivery/domain/repository/DeliveryRepository.java
@@ -1,7 +1,13 @@
 package com.sparta.msa.delivery.domain.repository;
 
+import com.sparta.msa.delivery.application.dto.UpdateDeliveryResponse;
 import com.sparta.msa.delivery.domain.model.Delivery;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface DeliveryRepository {
     Delivery save(Delivery delivery);
+
+    Optional<Delivery> findByUuid(UUID uuid);
 }
