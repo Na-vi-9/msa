6539334ed5333ea8077507e1861cfe1c7bfdcd25
soo1e <gameswuon@gmail.com>
2024-12-11 02:33:01 +0900

    UUID로 변경 및 기존의 UpdateOrderRequest.java와 CreateOrderRequest.java를 OrderRequest로 통합

--- order/src/main/java/com/sparta/msa/order/application/dto/OrderRequest.java
+++ order/src/main/java/com/sparta/msa/order/application/dto/OrderRequest.java
@@ -4,10 +4,10 @@ import lombok.Data;
 import java.util.UUID;
 
 @Data
-public class CreateOrderRequest {
-    private UUID supplierCompanyUUID;
-    private UUID receiverCompanyUUID;
-    private UUID productUUID;
+public class OrderRequest {
+    private UUID supplierCompanyID;
+    private UUID receiverCompanyID;
+    private UUID productID;
     private Integer quantity;
     private String memo;
 }
