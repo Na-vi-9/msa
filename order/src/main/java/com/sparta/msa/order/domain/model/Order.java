767e733b1014c9735ff88feb7a07f9b225105d41
soo1e <gameswuon@gmail.com>
2024-12-11 02:32:33 +0900

    UUID로 변경 및 Setter 삭제 후 Builder로 변경

--- order/src/main/java/com/sparta/msa/order/domain/model/Order.java
+++ order/src/main/java/com/sparta/msa/order/domain/model/Order.java
@@ -1,44 +1,35 @@
 package com.sparta.msa.order.domain.model;
 
-import com.sparta.msa.order.application.dto.CreateOrderRequest;
+import com.sparta.msa.order.application.dto.OrderRequest;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
-@Setter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
 @Entity
-@EntityListeners(AuditingEntityListener.class) // JPA Auditing 활성화
 @Table(name = "p_order")
 public class Order {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
+    @Column(name = "uuid", nullable = false, updatable = false)
     private UUID uuid;
 
     @Column(name = "supplier_company_id", nullable = false)
-    private Long supplierCompanyId;
+    private UUID supplierCompanyId;
 
     @Column(name = "receiver_company_id", nullable = false)
-    private Long receiverCompanyId;
-
-    @Column(name = "order_id", nullable = false, unique = true)
-    private Long orderId;
+    private UUID receiverCompanyId;
 
     @Column(name = "product_id", nullable = false)
-    private Long productId;
+    private UUID productId;
 
     @Column(name = "delivery_id", nullable = false)
-    private Long deliveryId;
+    private UUID deliveryId;
 
     @Column(nullable = false)
     private Integer quantity;
@@ -47,17 +38,16 @@ public class Order {
     private String memo;
 
     @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
     private Boolean isDeleted = false;
 
     @Column(name = "created_at", nullable = false, updatable = false)
-    @CreatedDate
     private LocalDateTime createdAt;
 
     @Column(name = "created_by", nullable = false)
-    private String createdBy;
+    private String createdBy = "system";
 
     @Column(name = "updated_at")
-    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     @Column(name = "updated_by")
@@ -69,26 +59,35 @@ public class Order {
     @Column(name = "deleted_by")
     private String deletedBy;
 
-    // 엔티티 생성 메서드
-    public static Order createOrder(CreateOrderRequest request, Long supplierCompanyId, Long receiverCompanyId, Long productId, Long deliveryId) {
-        Order order = new Order();
-        order.uuid = UUID.randomUUID();
-        order.orderId = System.currentTimeMillis(); // 임시로 고유한 값 생성
-        order.supplierCompanyId = supplierCompanyId;
-        order.receiverCompanyId = receiverCompanyId;
-        order.productId = productId;
-        order.deliveryId = deliveryId;
-        order.quantity = request.getQuantity();
-        order.memo = request.getMemo();
-        order.createdBy = "system"; // 유저 도메인 구현 전 임시값
-        order.updatedBy = "system"; // 유저 도메인 구현 전 임시값
-        return order;
+    @Builder
+    public static Order createOrder(OrderRequest request, UUID supplierCompanyId, UUID receiverCompanyId, UUID productId, UUID deliveryId) {
+        return Order.builder()
+                .uuid(UUID.randomUUID())
+                .supplierCompanyId(supplierCompanyId)
+                .receiverCompanyId(receiverCompanyId)
+                .productId(productId)
+                .deliveryId(deliveryId)
+                .quantity(request.getQuantity())
+                .memo(request.getMemo())
+                .createdAt(LocalDateTime.now())
+                .createdBy("system") // 임시
+                .updatedBy("system") // 임시
+                .build();
     }
 
-    // soft-delete
     public void delete(String deletedBy) {
         this.isDeleted = true;
         this.deletedAt = LocalDateTime.now();
         this.deletedBy = deletedBy;
+    }
+
+    public void updateOrder(UUID supplierCompanyId, UUID receiverCompanyId, UUID productId, Integer quantity, String memo, String updatedBy) {
+        this.supplierCompanyId = supplierCompanyId;
+        this.receiverCompanyId = receiverCompanyId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.memo = memo;
+        this.updatedBy = updatedBy;
+        this.updatedAt = LocalDateTime.now();
     }
 }
