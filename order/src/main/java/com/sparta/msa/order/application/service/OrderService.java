758512940d6815fbfdcf4f68ddb6bd95fe6beadc
soo1e <gameswuon@gmail.com>
2024-12-11 11:00:08 +0900

    UUID 변경으로 인한 수정 및 조회 부분 API 명세서에 맞게 수정

--- order/src/main/java/com/sparta/msa/order/application/service/OrderService.java
+++ order/src/main/java/com/sparta/msa/order/application/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.sparta.msa.order.application.service;
 
+import com.sparta.msa.order.application.dto.OrderDetailResponse;
+import com.sparta.msa.order.application.dto.OrderListResponse;
 import com.sparta.msa.order.application.dto.OrderRequest;
 import com.sparta.msa.order.application.dto.OrderResponse;
 import com.sparta.msa.order.domain.model.Order;
@@ -23,20 +25,20 @@ public class OrderService {
 
     // 주문 생성
     public OrderResponse createOrder(OrderRequest request) {
-        UUID supplierCompanyId = request.getSupplierCompanyID();
-        UUID receiverCompanyId = request.getReceiverCompanyID();
-        UUID productId = request.getProductID();
-        UUID deliveryId = UUID.randomUUID(); // 임시 UUID 생성
+        UUID supplierCompanyUUID = request.getSupplierCompanyUUID();
+        UUID receiverCompanyUUID = request.getReceiverCompanyUUID();
+        UUID productUUID = request.getProductUUID();
+        UUID deliveryUUID = UUID.randomUUID(); // 임시 UUID 생성
 
         // 값이 null인지 확인
-        if (supplierCompanyId == null || receiverCompanyId == null || productId == null) {
+        if (supplierCompanyUUID == null || receiverCompanyUUID == null || productUUID == null) {
             throw new CustomException(ErrorCode.BAD_REQUEST, "필수 데이터가 누락되었습니다.");
         }
 
         try {
-            Order order = Order.createOrder(request, supplierCompanyId, receiverCompanyId, productId, deliveryId);
+            Order order = Order.createOrder(request, supplierCompanyUUID, receiverCompanyUUID, productUUID, deliveryUUID);
             orderRepository.save(order);
-            return new OrderResponse(order.getUuid(), order.getDeliveryId());
+            return new OrderResponse(order.getUuid(), order.getDeliveryUUID());
         } catch (Exception e) {
             e.printStackTrace();
             throw new CustomException(ErrorCode.ORDER_CREATION_FAILED, "DB 저장 중 문제가 발생했습니다.");
@@ -45,23 +47,39 @@ public class OrderService {
     }
 
     // 주문 목록 조회
-    public Page<OrderResponse> getOrders(String condition, Pageable pageable) {
+    public Page<OrderListResponse> getOrders(String condition, Pageable pageable) {
         Page<Order> orders = orderRepository.findAllWithCondition(condition, null, pageable);
 
         if (orders.isEmpty()) {
             throw new CustomException(ErrorCode.NOT_FOUND, "주문 목록이 비어 있습니다.");
         }
 
-        return orders.map(order -> new OrderResponse(order.getUuid(), order.getDeliveryId()));
+        return orders.map(order -> OrderListResponse.builder()
+                .uuid(order.getUuid())
+                .supplierCompanyUUID(order.getSupplierCompanyUUID())
+                .receiverCompanyUUID(order.getReceiverCompanyUUID())
+                .productUUID(order.getProductUUID())
+                .quantity(order.getQuantity())
+                .memo(order.getMemo())
+                .build());
     }
 
+
     // 주문 단건 조회
-    public OrderResponse getOrderDetail(UUID orderUUID) {
+    public OrderDetailResponse getOrderDetail(UUID orderUUID) {
         Order order = orderRepository.findByUuidAndIsDeletedFalse(orderUUID)
                 .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
 
-        return new OrderResponse(order.getUuid(), order.getDeliveryId());
+        return OrderDetailResponse.builder()
+                .supplierCompanyUUID(order.getSupplierCompanyUUID())
+                .receiverCompanyUUID(order.getReceiverCompanyUUID())
+                .productUUID(order.getProductUUID())
+                .quantity(order.getQuantity())
+                .memo(order.getMemo())
+                .deliveryUUID(order.getDeliveryUUID())
+                .build();
     }
+
 
     // 주문 수정
     public OrderResponse updateOrder(UUID orderUUID, OrderRequest request) {
@@ -69,9 +87,9 @@ public class OrderService {
                 .orElseThrow(() -> new CustomException(ErrorCode.ORDER_NOT_FOUND));
 
         order.updateOrder(
-                request.getSupplierCompanyID(),
-                request.getReceiverCompanyID(),
-                request.getProductID(),
+                request.getSupplierCompanyUUID(),
+                request.getReceiverCompanyUUID(),
+                request.getProductUUID(),
                 request.getQuantity(),
                 request.getMemo(),
                 "system"
@@ -79,7 +97,7 @@ public class OrderService {
 
         Order updatedOrder = orderRepository.save(order);
 
-        return new OrderResponse(updatedOrder.getUuid(), updatedOrder.getDeliveryId());
+        return new OrderResponse(updatedOrder.getUuid(), updatedOrder.getDeliveryUUID());
     }
 
     // 주문 삭제
