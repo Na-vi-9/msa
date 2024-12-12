85d66ed0e385f1fc552eb962c89bf405be102a68
rua0704 <rua0704@gmail.com>
2024-12-12 16:13:36 +0900

    fix - 배송생성에 배송경로 생성 로직 추가 & TODO 작성

--- delivery/src/main/java/com/sparta/msa/delivery/application/service/DeliveryService.java
+++ delivery/src/main/java/com/sparta/msa/delivery/application/service/DeliveryService.java
@@ -1,7 +1,8 @@
 package com.sparta.msa.delivery.application.service;
 
 import com.querydsl.core.types.Predicate;
-import com.sparta.msa.delivery.application.dto.*;
+import com.sparta.msa.delivery.application.dto.delivery.*;
+import com.sparta.msa.delivery.application.dto.deliveryRoute.CreateDeliveryRouteDto;
 import com.sparta.msa.delivery.domain.model.Delivery;
 import com.sparta.msa.delivery.domain.repository.DeliveryRepository;
 import com.sparta.msa.delivery.infrastructure.exception.CustomException;
@@ -25,18 +26,26 @@ public class DeliveryService {
 
     @Transactional
     public CreateDeliveryResponse createDelivery(CreateDeliveryDto request) {
-        // TODO 경로 생성 return 배송 담당자
-        String deliveryManagerUsername = "username";
+        // TODO
+        //  업체 배송 담당자 배정
+        String companyDeliveryManagerUsername = "username";
+        Delivery delivery = deliveryRepository.save(
+                Delivery.create(request.getOrderUUID(), request.getStatus(), request.getDepartureHubUUID(),
+                        request.getArrivalHubUUID(), request.getDeliveryAddress(),
+                        request.getRecipientUsername(), companyDeliveryManagerUsername)
+        );
+        deliveryRouteService.createDeliveryRoute(
+                CreateDeliveryRouteDto.create(delivery.getUuid(), request.getDepartureHubUUID(), request.getArrivalHubUUID())
+        );
 
-        Delivery delivery = Delivery.create(request.getOrderUUID(), request.getStatus(), request.getDepartureHubUUID(), request.getArrivalHubUUID(), request.getDeliveryAddress(), request.getRecipientUsername(), deliveryManagerUsername);
-
-        return CreateDeliveryResponse.of(deliveryRepository.save(delivery));
+        return CreateDeliveryResponse.of(delivery);
 
     }
 
     @Transactional
     public UpdateDeliveryResponse updateDelivery(UUID deliveryUUID, UpdateDeliveryDto request) {
-
+        // TODO
+        //  배송완료 상태 변경시 배송경로들 Status 모두 변경
         return deliveryRepository.findByUuidAndIsDeletedFalse(deliveryUUID).map(delivery -> {
             delivery.update(request.getOrderUUID(), request.getStatus(), request.getDepartureHubUUID(), request.getArrivalHubUUID(), request.getDeliveryAddress(), request.getRecipientUsername(), request.getDeliveryManagerUsername());
             return UpdateDeliveryResponse.of(delivery);
@@ -46,6 +55,8 @@ public class DeliveryService {
 
     @Transactional
     public void deleteDelivery(UUID deliveryUUID, String deleteBy) {
+        // TODO
+        //  배송 삭제시 경로들 모두 delete 처리
         deliveryRepository.findByUuidAndIsDeletedFalse(deliveryUUID)
                 .orElseThrow(() -> new CustomException(ErrorCode.DELIVERY_NOT_FOUND))
                 .delete(deleteBy);
