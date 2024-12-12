0f709b8d720c4c72e7e84d5a2b0e414544d73e11
rua0704 <rua0704@gmail.com>
2024-12-12 21:32:28 +0900

    feat - 배송 경로 검색 및 조회 구현

--- delivery/src/main/java/com/sparta/msa/delivery/domain/repository/DeliveryRouteRepository.java
+++ delivery/src/main/java/com/sparta/msa/delivery/domain/repository/DeliveryRouteRepository.java
@@ -1,6 +1,9 @@
 package com.sparta.msa.delivery.domain.repository;
 
+import com.querydsl.core.types.Predicate;
 import com.sparta.msa.delivery.domain.model.DeliveryRoute;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +12,7 @@ import java.util.UUID;
 @Repository
 public interface DeliveryRouteRepository {
     DeliveryRoute save(DeliveryRoute deliveryRoute);
+    Page<DeliveryRoute> searchDeliveryRouteIsDeletedFalse(Predicate predicate, Pageable pageable);
 
     Optional<DeliveryRoute> findByUuidAndIsDeletedFalse(UUID uuid);
 }
--- delivery/src/main/java/com/sparta/msa/delivery/presentation/controller/DeliveryRouteController.java
+++ delivery/src/main/java/com/sparta/msa/delivery/presentation/controller/DeliveryRouteController.java
@@ -1,12 +1,19 @@
 package com.sparta.msa.delivery.presentation.controller;
 
+import com.querydsl.core.types.Predicate;
+import com.sparta.msa.delivery.application.dto.deliveryRoute.DeliveryRouteResponse;
 import com.sparta.msa.delivery.application.dto.deliveryRoute.UpdateDeliveryRouteResponse;
 import com.sparta.msa.delivery.application.service.DeliveryRouteService;
 import com.sparta.msa.delivery.common.dto.CommonResponse;
+import com.sparta.msa.delivery.domain.model.DeliveryRoute;
 import com.sparta.msa.delivery.presentation.request.UpdateDeliveryRouteRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -24,5 +31,16 @@ public class DeliveryRouteController {
 
         return CommonResponse.ofSuccess(deliveryRouteService.updateDeliveryRoute(deliveryRouteUUID, request.toDto()));
 
+    }
+
+    @GetMapping
+    public CommonResponse<PagedModel<DeliveryRouteResponse>> searchDeliveryRoutes(@QuerydslPredicate(root = DeliveryRoute.class) Predicate predicate,
+                                                                         @PageableDefault(sort = {"createdAt", "updatedAt"}) Pageable pageable) {
+        return CommonResponse.ofSuccess(deliveryRouteService.searchDeliveryRoutesIsDeletedFalse(predicate, pageable));
+    }
+
+    @GetMapping("/{DeliveryRouteUUID}")
+    public CommonResponse<DeliveryRouteResponse> findDeliveryRouteByUUID(@PathVariable UUID DeliveryRouteUUID) {
+        return CommonResponse.ofSuccess(deliveryRouteService.findDeliveryRouteByUUID(DeliveryRouteUUID));
     }
 }
