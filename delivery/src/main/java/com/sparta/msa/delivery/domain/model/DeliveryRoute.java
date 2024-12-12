5a64262950bd777251e3f427b60f760e2ee7cb2e
rua0704 <rua0704@gmail.com>
2024-12-12 12:05:25 +0900

    feat - DeliveryRoute entity 생성

---
+++ delivery/src/main/java/com/sparta/msa/delivery/domain/model/DeliveryRoute.java
@@ -0,0 +1,81 @@
+package com.sparta.msa.delivery.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_delivery_route")
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+public class DeliveryRoute extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "deliver_uuid", nullable = false)
+    private UUID deliverUUID;
+
+    @Column(name = "departure_hub_uuid", nullable = false)
+    private UUID departureHubUUID;
+
+    @Column(name = "arrival_hub_uuid", nullable = false)
+    private UUID arrivalHubUUID;
+
+    @Column(name = "sequence", nullable = false)
+    private Integer sequence;
+
+    @Column(name = "estimated_distance_km", nullable = false)
+    private Double estimatedDistanceKm;
+
+    @Column(name = "estimated_time_min", nullable = false)
+    private Integer estimatedTimeMin;
+
+    @Column(name = "actual_distance_km")
+    private Double actualDistanceKm;
+
+    @Column(name = "actual_time_min")
+    private Integer actualTimeMin;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private DeliveryStatus status;
+
+    @Column(name = "delivery_manager_username", nullable = false, length = 10)
+    private String deliveryManagerUsername;
+
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    private Boolean isDeleted = false;
+
+    @Column(name = "deleted_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by", length = 10)
+    private String deletedBy;
+
+    public static DeliveryRoute create(UUID deliveryUUID,
+                                       UUID departureHubUUID,
+                                       UUID arrivalHubUUID,
+                                       Integer sequence,
+                                       Double estimatedDistanceKm,
+                                       Integer estimatedTimeMin,
+                                       DeliveryStatus status,
+                                       String deliveryManagerUsername) {
+        return DeliveryRoute.builder()
+                .deliverUUID(deliveryUUID)
+                .departureHubUUID(departureHubUUID)
+                .arrivalHubUUID(arrivalHubUUID)
+                .sequence(sequence)
+                .estimatedDistanceKm(estimatedDistanceKm)
+                .estimatedTimeMin(estimatedTimeMin)
+                .status(status)
+                .deliveryManagerUsername(deliveryManagerUsername)
+                .build();
+    }
+}
