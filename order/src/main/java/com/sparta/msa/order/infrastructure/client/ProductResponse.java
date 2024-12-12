69c8bce7170404c62aaa767424848a771f7171c2
soo1e <gameswuon@gmail.com>
2024-12-12 17:05:43 +0900

    통신을 위한 dto 생성

---
+++ order/src/main/java/com/sparta/msa/order/infrastructure/client/ProductResponse.java
@@ -0,0 +1,18 @@
+package com.sparta.msa.order.infrastructure.client;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+    private UUID productUUID;
+    private String name;
+    private Integer quantity;
+    private UUID companyUUID;
+    private UUID hubUUID;
+}
