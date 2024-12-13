d5b2d2a5642705f8fa79aa33c4f7279e2b122a0c
soo1e <gameswuon@gmail.com>
2024-12-13 10:57:24 +0900

    배송 담당자 배정 추가

--- delivery/src/main/java/com/sparta/msa/delivery/application/service/DeliveryManagerService.java
+++ delivery/src/main/java/com/sparta/msa/delivery/application/service/DeliveryManagerService.java
@@ -3,6 +3,8 @@ package com.sparta.msa.delivery.application.service;
 import com.sparta.msa.delivery.application.dto.deliveryManager.DeliveryManagerRequest;
 import com.sparta.msa.delivery.application.dto.deliveryManager.DeliveryManagerResponse;
 import com.sparta.msa.delivery.domain.model.DeliveryManager;
+import com.sparta.msa.delivery.infrastructure.exception.CustomException;
+import com.sparta.msa.delivery.infrastructure.exception.ErrorCode;
 import com.sparta.msa.delivery.infrastructure.repository.DeliveryManagerJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,18 @@ public class DeliveryManagerService {
                 nextOrder,
                 request.getUsername()
         );
+
+        deliveryManagerJpaRepository.save(deliveryManager);
+        return new DeliveryManagerResponse(deliveryManager);
+    }
+
+    @Transactional
+    public DeliveryManagerResponse assignDeliveryManager() {
+        DeliveryManager deliveryManager = deliveryManagerJpaRepository.findTopByIsDeletedFalseOrderByDeliveryOrderAsc()
+                .orElseThrow(() -> new CustomException(ErrorCode.DELIVERY_MANAGER_NOT_FOUND));
+
+        Integer lastOrder = deliveryManagerJpaRepository.findLastDeliveryOrder();
+        deliveryManager.updateDeliveryOrder((lastOrder != null) ? lastOrder + 1 : 1);
 
         deliveryManagerJpaRepository.save(deliveryManager);
         return new DeliveryManagerResponse(deliveryManager);
