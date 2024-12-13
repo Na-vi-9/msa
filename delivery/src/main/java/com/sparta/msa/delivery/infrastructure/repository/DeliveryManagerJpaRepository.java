539e7bc5415115fd1385d39e528a9b8229507a8b
soo1e <gameswuon@gmail.com>
2024-12-13 11:41:19 +0900

    레포지토리 내 배송 담당자 검색 메서드 구현

--- delivery/src/main/java/com/sparta/msa/delivery/infrastructure/repository/DeliveryManagerJpaRepository.java
+++ delivery/src/main/java/com/sparta/msa/delivery/infrastructure/repository/DeliveryManagerJpaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -16,4 +17,9 @@ public interface DeliveryManagerJpaRepository extends JpaRepository<DeliveryMana
 
     @Query("SELECT MAX(dm.deliveryOrder) FROM DeliveryManager dm WHERE dm.isDeleted = false")
     Integer findLastDeliveryOrder();
+
+    @Query("SELECT dm FROM DeliveryManager dm WHERE " +
+            "(:condition IS NULL OR dm.type = :condition) AND " +
+            "(:keyword IS NULL OR dm.username LIKE %:keyword%)")
+    List<DeliveryManager> search(String condition, String keyword);
 }
