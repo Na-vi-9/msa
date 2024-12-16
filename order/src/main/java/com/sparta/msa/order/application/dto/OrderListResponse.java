4bec58b0b3907da495947db89cb9419896a5cb89
soo1e <gameswuon@gmail.com>
2024-12-16 20:05:46 +0900

    유저에 따른 dto 변경

--- order/src/main/java/com/sparta/msa/order/application/dto/OrderListResponse.java
+++ order/src/main/java/com/sparta/msa/order/application/dto/OrderListResponse.java
@@ -1,17 +1,36 @@
 package com.sparta.msa.order.application.dto;
 
-import lombok.Builder;
+import com.sparta.msa.order.domain.model.Order;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
-@Builder
 public class OrderListResponse {
+
     private final UUID uuid;
     private final UUID supplierCompanyUUID;
     private final UUID receiverCompanyUUID;
     private final UUID productUUID;
     private final Integer quantity;
     private final String memo;
+
+    public OrderListResponse(UUID uuid, UUID supplierCompanyUUID, UUID receiverCompanyUUID,
+                             UUID productUUID, Integer quantity, String memo) {
+        this.uuid = uuid;
+        this.supplierCompanyUUID = supplierCompanyUUID;
+        this.receiverCompanyUUID = receiverCompanyUUID;
+        this.productUUID = productUUID;
+        this.quantity = quantity;
+        this.memo = memo;
+    }
+
+    public OrderListResponse(Order order) {
+        this.uuid = order.getUuid();
+        this.supplierCompanyUUID = order.getSupplierCompanyUUID();
+        this.receiverCompanyUUID = order.getReceiverCompanyUUID();
+        this.productUUID = order.getProductUUID();
+        this.quantity = order.getQuantity();
+        this.memo = order.getMemo();
+    }
 }
