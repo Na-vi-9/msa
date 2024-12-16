3115c656c20c5b73952ce8c323178c0499077284
eleunadeu <eleunadeu@gmail.com>
2024-12-16 14:01:29 +0900

    설정 파일 및 Exception 파일 추가

---
+++ company/src/main/java/com/sparta/msa/company/domain/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.sparta.msa.company.domain.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. (%s)"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),
+    NOT_FOUND_COMPANY(HttpStatus.NOT_FOUND, "업체 UUID: %s를 찾을 수 없습니다."),
+    DELETED_COMPANY(HttpStatus.BAD_REQUEST, "삭제된 업체입니다.");
+
+    private final HttpStatus status;
+    private final String description;
+
+    ErrorCode(HttpStatus status, String description) {
+        this.status = status;
+        this.description = description;
+    }
+
+}
