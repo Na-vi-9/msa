9caae47d26a7d369c182b22a7e2b5b08e15c42c1
soo1e <gameswuon@gmail.com>
2024-12-16 23:47:06 +0900

    aifeignclient 생성

---
+++ order/src/main/java/com/sparta/msa/order/infrastructure/client/AiFeignClient.java
@@ -0,0 +1,17 @@
+package com.sparta.msa.order.infrastructure.client;
+
+import com.sparta.msa.order.infrastructure.configuration.FeignConfig;
+import com.sparta.msa.order.presentation.request.AiMessageRequestDto;
+import com.sparta.msa.order.presentation.request.GeminiClientRequestDto;
+import com.sparta.msa.order.presentation.response.AiMessageCreateResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "ai", url = "http://localhost:19099", configuration = FeignConfig.class)
+public interface AiFeignClient {
+
+    @PostMapping("/ai")
+    AiMessageCreateResponseDto createAiMessage(@RequestBody GeminiClientRequestDto request);
+}
+
