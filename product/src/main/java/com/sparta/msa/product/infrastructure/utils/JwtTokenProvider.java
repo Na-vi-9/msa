7f3dc1af3b5604443d384f28be59af3d17f64817
eunjeong <jjj3080@naver.com>
2024-12-16 22:32:24 +0900

    feature/67/product/권한 설정

--- product/src/main/java/com/sparta/msa/product/infrastructure/utils/JwtTokenProvider.java
+++ product/src/main/java/com/sparta/msa/product/infrastructure/utils/JwtTokenProvider.java
@@ -2,11 +2,8 @@ package com.sparta.msa.product.infrastructure.utils;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +11,6 @@ import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
-@Slf4j
 @Component
 public class JwtTokenProvider {
 
@@ -35,27 +31,7 @@ public class JwtTokenProvider {
         key = Keys.hmacShaKeyFor(bytes);
     }
 
-    public String createAccessToken(String username, String role) {
-        return Jwts.builder()
-                .claim("username", username)
-                .claim("role", role)
-                .setIssuer(issuer)
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + accessExpiration))
-                .signWith(key, SignatureAlgorithm.HS512)
-                .compact(); // JWT 반환
-    }
-
-    // JWT 토큰을 요청에서 추출
-    public String getTokenFromRequest(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        if (token != null && token.startsWith("Bearer ")) {
-            token = token.substring(7);
-            log.debug("Extracted token: {}", token);
-        }
-        return token;
-    }
-
+    // 토큰에서 Claims 추출
     public Claims getClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
@@ -64,29 +40,21 @@ public class JwtTokenProvider {
                 .getBody();
     }
 
-    public boolean isTokenExpired(String token) {
-        String jwtToken = extractToken(token);
-        Claims claims = getClaims(jwtToken);
-        return claims.getExpiration().before(new Date()); // 만료된 토큰 체크
-    }
-
+    // 토큰에서 username 추출
     public String getUsernameFromToken(String token) {
-        String jwtToken = extractToken(token);
-        Claims claims = getClaims(jwtToken);
+        Claims claims = getClaims(token);
         return claims.get("username", String.class);
     }
 
+    // 토큰에서 role 추출
     public String getRoleFromToken(String token) {
-        String jwtToken = extractToken(token);
-        Claims claims = getClaims(jwtToken);
+        Claims claims = getClaims(token);
         return claims.get("role", String.class);
     }
 
-    public String extractToken(String token) {
-        // "Bearer "로 시작하는지 확인
-        if (token != null && token.startsWith("Bearer ")) {
-            return token.substring(7);
-        }
-        return token;
+    // 토큰이 만료됐는지 확인
+    public boolean isTokenExpired(String token) {
+        Claims claims = getClaims(token);
+        return claims.getExpiration().before(new Date());
     }
 }
