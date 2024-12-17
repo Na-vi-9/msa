b99b9b053f8f957762c25e0eb34de57ab9807f73
eleunadeu <eleunadeu@gmail.com>
2024-12-17 14:42:48 +0900

    Feat: Hub 인가 기능 구현 #60 - Hub/Hub Route 시 유저 인가 기능 구현

--- hub/src/main/java/com/sparta/msa/hub/application/service/HubRouteService.java
+++ hub/src/main/java/com/sparta/msa/hub/application/service/HubRouteService.java
@@ -2,13 +2,17 @@ package com.sparta.msa.hub.application.service;
 
 import com.sparta.msa.hub.application.dto.HubRouteInfo;
 import com.sparta.msa.hub.application.dto.HubRouteResponse;
+import com.sparta.msa.hub.application.dto.UserDto;
 import com.sparta.msa.hub.domain.entity.Hub;
 import com.sparta.msa.hub.domain.entity.HubRoute;
+import com.sparta.msa.hub.domain.enums.Role;
 import com.sparta.msa.hub.domain.exception.CustomException;
 import com.sparta.msa.hub.domain.exception.ErrorCode;
+import com.sparta.msa.hub.infrastructure.clients.UserClient;
 import com.sparta.msa.hub.infrastructure.repository.HubRepository;
 import com.sparta.msa.hub.infrastructure.repository.HubRouteRepository;
 import com.sparta.msa.hub.presentation.dto.HubInfoRequest;
+import com.sparta.msa.hub.presentation.dto.UserInfoRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
@@ -22,7 +26,6 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -32,39 +35,45 @@ public class HubRouteService {
 
     private final HubRouteRepository hubRouteRepository;
     private final HubRepository hubRepository;
+    private final UserClient userClient;
     private final RestTemplate restTemplate;
 
     @Value("${service.api.key}")
     private String apiKey;
 
     // TODO: repository 중복 코드 메서드로 최소화 하기 + 검증 메서드 만들기
-    public void calculateAndSaveAllHubRoutes() {
-        List<Hub> hubs = hubRepository.findAll();
-        int requestCount = 1;  // 요청 횟수를 추적
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    public void calculateAndSaveAllHubRoutes(UserInfoRequest userInfo) {
+        Role userRole = validateUsername(userInfo).getRole();
+        if (userRole.equals(Role.MASTER)) {
+            List<Hub> hubs = hubRepository.findAll();
+            int requestCount = 1;  // 요청 횟수를 추적
+            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
-        for (int i = 0; i < hubs.size(); i++) {
-            for (int j = i + 1; j < hubs.size(); j++) {
-                UUID departureHubUUID = hubs.get(i).getHubUUID();
-                UUID arrivalHubUUID = hubs.get(j).getHubUUID();
+            for (int i = 0; i < hubs.size(); i++) {
+                for (int j = i + 1; j < hubs.size(); j++) {
+                    UUID departureHubUUID = hubs.get(i).getHubUUID();
+                    UUID arrivalHubUUID = hubs.get(j).getHubUUID();
 
-                calculateAndSaveHubRoute(departureHubUUID, arrivalHubUUID);
+                    calculateAndSaveHubRoute(departureHubUUID, arrivalHubUUID);
 
-                requestCount++;
+                    requestCount++;
 
-                // TODO: 효율적으로 API 분당 제한 시간동안 대기하도록 검토
-                // 요청이 40번을 초과하면 1분 대기
-                if (requestCount >= 39) {
-                    try {
-                        System.out.println("Rate limit reached. Waiting for 1 minute...");
-                        Thread.sleep(60_000);  // 1분 대기
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();  // 인터럽트 상태 복구
+                    // TODO: 효율적으로 API 분당 제한 시간동안 대기하도록 검토
+                    // 요청이 40번을 초과하면 1분 대기
+                    if (requestCount >= 39) {
+                        try {
+                            System.out.println("Rate limit reached. Waiting for 1 minute...");
+                            Thread.sleep(60_000);  // 1분 대기
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();  // 인터럽트 상태 복구
+                        }
+                        requestCount = 0;  // 요청 횟수 초기화
                     }
-                    requestCount = 0;  // 요청 횟수 초기화
                 }
             }
         }
+
+        throw new CustomException(ErrorCode.ACCESS_DENIED);
     }
 
     public HubRouteResponse getHubRoute(UUID hubRouteUUID) {
@@ -81,35 +90,44 @@ public class HubRouteService {
                 .collect(Collectors.toList());
     }
 
-    public HubRouteResponse updateHubRoute(UUID hubRouteUUID) {
-        HubRoute hubRoute = hubRouteRepository.findByHubRouteUUIDAndIsDeletedIsFalse(hubRouteUUID)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB_ROUTE, hubRouteUUID));
+    public HubRouteResponse updateHubRoute(UUID hubRouteUUID, UserInfoRequest userInfo) {
+        Role userRole = validateUsername(userInfo).getRole();
+        if (userRole.equals(Role.MASTER)) {
+            HubRoute hubRoute = hubRouteRepository.findByHubRouteUUIDAndIsDeletedIsFalse(hubRouteUUID)
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB_ROUTE, hubRouteUUID));
 
-        Hub departureHub = hubRepository.findByHubUUID(hubRoute.getDepartureHubUUID())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB, hubRoute.getDepartureHubUUID()));
-        Hub arrivalHub = hubRepository.findByHubUUID(hubRoute.getArrivalHubUUID())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB, hubRoute.getArrivalHubUUID()));
+            Hub departureHub = hubRepository.findByHubUUID(hubRoute.getDepartureHubUUID())
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB, hubRoute.getDepartureHubUUID()));
+            Hub arrivalHub = hubRepository.findByHubUUID(hubRoute.getArrivalHubUUID())
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB, hubRoute.getArrivalHubUUID()));
 
-        String url = "https://api.openrouteservice.org/v2/directions/driving-car";
-        String response = callApi(url, departureHub.getLatitude(), departureHub.getLongitude(),
-                arrivalHub.getLatitude(), arrivalHub.getLongitude());
+            String url = "https://api.openrouteservice.org/v2/directions/driving-car";
+            String response = callApi(url, departureHub.getLatitude(), departureHub.getLongitude(),
+                    arrivalHub.getLatitude(), arrivalHub.getLongitude());
 
-        double distance = extractDistanceFromGeoJson(response);
-        double duration = extractDurationFromGeoJson(response);
+            double distance = extractDistanceFromGeoJson(response);
+            double duration = extractDurationFromGeoJson(response);
 
-        hubRoute.update((int) duration, distance);
+            hubRoute.update((int) duration, distance);
 
-        return HubRouteResponse.from(hubRoute);
+            return HubRouteResponse.from(hubRoute);
+        }
+
+        throw new CustomException(ErrorCode.ACCESS_DENIED);
     }
 
-    public void deleteHubRoute(UUID hubRouteUUID) {
-        HubRoute hubRoute = hubRouteRepository.findByHubRouteUUIDAndIsDeletedIsFalse(hubRouteUUID)
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB_ROUTE, hubRouteUUID));
+    public void deleteHubRoute(UUID hubRouteUUID, UserInfoRequest userInfo) {
+        UserDto userDto = validateUsername(userInfo);
+        if (userDto.getRole().equals(Role.MASTER)) {
+            HubRoute hubRoute = hubRouteRepository.findByHubRouteUUIDAndIsDeletedIsFalse(hubRouteUUID)
+                    .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_HUB_ROUTE, hubRouteUUID));
 
-        // TODO: DeletedUsername 추가되도록 변경
-        String deletedManagerName = "master";
+            hubRoute.markDeleted(userDto.getUsername());
+        }
 
-        hubRoute.markDeleted(deletedManagerName);
+
+
+        throw new CustomException(ErrorCode.ACCESS_DENIED);
     }
 
     public HubRouteInfo getRouteByDepartureAndArrival(HubInfoRequest hubInfoRequest) {
@@ -176,5 +194,9 @@ public class HubRouteService {
             log.error("에러 발생: {}", e.getMessage(), e);
             return 0;
         }
+    }
+
+    private UserDto validateUsername(UserInfoRequest request) {
+        return userClient.getUserInfo(request.getToken(), request.getUsername()).data();
     }
 }
--- hub/src/main/java/com/sparta/msa/hub/presentation/controller/HubRouteController.java
+++ hub/src/main/java/com/sparta/msa/hub/presentation/controller/HubRouteController.java
@@ -3,7 +3,11 @@ package com.sparta.msa.hub.presentation.controller;
 import com.sparta.msa.hub.application.dto.HubRouteInfo;
 import com.sparta.msa.hub.application.dto.HubRouteResponse;
 import com.sparta.msa.hub.application.service.HubRouteService;
+import com.sparta.msa.hub.domain.enums.Role;
+import com.sparta.msa.hub.domain.exception.CustomException;
+import com.sparta.msa.hub.domain.exception.ErrorCode;
 import com.sparta.msa.hub.presentation.dto.HubInfoRequest;
+import com.sparta.msa.hub.presentation.dto.UserInfoRequest;
 import com.sparta.msa.hub.presentation.exception.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -20,18 +24,26 @@ public class HubRouteController {
 
     /* 허브 전체 경로 저장은 API 무료에서 제한이 있기 때문에 미사용 처리*/
     @PostMapping("/all")
-    public CommonResponse<String> generateRouteInfo() {
-        hubRouteService.calculateAndSaveAllHubRoutes();
+    public CommonResponse<String> generateRouteInfo(@RequestHeader("Authorization") String token,
+                                                    @RequestHeader("X-Username") String username,
+                                                    @RequestHeader("X-Role") String role) {
+
+        UserInfoRequest userInfo = UserInfoRequest.of(token, username, role);
+        hubRouteService.calculateAndSaveAllHubRoutes(userInfo);
         return CommonResponse.ofSuccess("모든 허브 간 이동 경로가 저장되었습니다.");
     }
 
     @PostMapping
-    public CommonResponse<HubRouteResponse> generateRouteInfo(@RequestBody HubInfoRequest hubInfoRequest) {
-        HubRouteResponse response = HubRouteResponse
-                .from(hubRouteService.calculateAndSaveHubRoute(
-                        hubInfoRequest.getDepartureHubUUID(),
-                        hubInfoRequest.getArrivalHubUUID()));
-        return CommonResponse.ofSuccess(response);
+    public CommonResponse<HubRouteResponse> generateRouteInfo(@RequestHeader("X-Role") String role,
+                                                              @RequestBody HubInfoRequest hubInfoRequest) {
+
+        if (role.equals(Role.MASTER.toString())) {
+            HubRouteResponse response = HubRouteResponse.from(hubRouteService.calculateAndSaveHubRoute(
+                    hubInfoRequest.getDepartureHubUUID(), hubInfoRequest.getArrivalHubUUID()));
+            return CommonResponse.ofSuccess(response);
+        }
+
+        throw new CustomException(ErrorCode.ACCESS_DENIED);
     }
 
     @GetMapping("/{hubRouteUUID}")
@@ -45,13 +57,23 @@ public class HubRouteController {
     }
 
     @PutMapping("/{hubRouteUUID}")
-    public CommonResponse<HubRouteResponse> updateHubRoute(@PathVariable UUID hubRouteUUID) {
-        return CommonResponse.ofSuccess(hubRouteService.updateHubRoute(hubRouteUUID));
+    public CommonResponse<HubRouteResponse> updateHubRoute(@RequestHeader("Authorization") String token,
+                                                           @RequestHeader("X-Username") String username,
+                                                           @RequestHeader("X-Role") String role,
+                                                           @PathVariable UUID hubRouteUUID) {
+
+        UserInfoRequest userInfo = UserInfoRequest.of(token, username, role);
+        return CommonResponse.ofSuccess(hubRouteService.updateHubRoute(hubRouteUUID, userInfo));
     }
 
     @DeleteMapping("/{hubRouteUUID}")
-    public CommonResponse<String> deleteHubRoute(@PathVariable UUID hubRouteUUID) {
-        hubRouteService.deleteHubRoute(hubRouteUUID);
+    public CommonResponse<String> deleteHubRoute(@RequestHeader("Authorization") String token,
+                                                 @RequestHeader("X-Username") String username,
+                                                 @RequestHeader("X-Role") String role,
+                                                 @PathVariable UUID hubRouteUUID) {
+
+        UserInfoRequest userInfo = UserInfoRequest.of(token, username, role);
+        hubRouteService.deleteHubRoute(hubRouteUUID, userInfo);
         return CommonResponse.ofSuccess("허브 경로를 삭제했습니다.");
     }
 
