6ced295955745ff9ee20ef95181d9b29a85bea7d
rua0704 <rua0704@gmail.com>
2024-12-17 15:32:31 +0900

    feat - 허브 정보 feign client

--- delivery/src/main/java/com/sparta/msa/delivery/application/service/HubService.java
+++ delivery/src/main/java/com/sparta/msa/delivery/application/service/HubService.java
@@ -1,9 +1,14 @@
 package com.sparta.msa.delivery.application.service;
 
+import com.sparta.msa.delivery.application.dto.hub.HubDto;
 import com.sparta.msa.delivery.application.dto.hubRoute.GetHubRouteRequest;
 import com.sparta.msa.delivery.application.dto.hubRoute.HubRouteDto;
 import com.sparta.msa.delivery.common.dto.CommonResponse;
 
+import java.util.UUID;
+
 public interface HubService {
     CommonResponse<HubRouteDto> getHubRoute(GetHubRouteRequest request);
+    CommonResponse<HubDto> getHub(UUID hubUUID);
+
 }
