a23eb164b327fae4a8b2cd2b737c2495871e753f
박용운 <121149088+eleunadeu@users.noreply.github.com>
2024-12-17 15:44:51 +0900

    Hub, Company User Feign Client 수정 (#79)
    * Fix: Hub, Company FeignClient 수정 #78
    - Hub, Company UserClient 호출 Endpoint 수정(/{username} -> /info)

--- hub/src/main/java/com/sparta/msa/hub/infrastructure/clients/UserClient.java
+++ hub/src/main/java/com/sparta/msa/hub/infrastructure/clients/UserClient.java
@@ -4,13 +4,13 @@ import com.sparta.msa.hub.application.dto.UserDto;
 import com.sparta.msa.hub.presentation.exception.CommonResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "users", url = "http://localhost:19097")
 public interface UserClient {
 
-    @GetMapping("/users/{username}")
+    @GetMapping("/users/info")
     CommonResponse<UserDto> getUserInfo(@RequestHeader("Authorization") String token,
-                                        @PathVariable("username") String username);
+                                        @RequestParam("username") String username);
 }
